e0127a91bbb39f2f451d4cee6db575909e571563
Tristan Matthews <le.businessman@gmail.com>
2014-01-09 01:28:47 -0500

    opus: always sync i_buffer with i_nb_samples (fixes #10135)
    Specifically, the crash at the end of playback was happening when end trim was
    not being subtracted from i_buffer.
    
    (cherry picked from commit f88f91467700484a65d6c5abf0a71cfc4a209320)
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/codec/opus.c
+++ modules/codec/opus.c
@@ -395,6 +395,10 @@ static block_t *DecodePacket( decoder_t *p_dec, ogg_packet *p_oggpacket,
             msg_Err( p_dec, "Error: corrupted stream?" );
         return NULL;
     }
+
+    p_aout_buffer->i_buffer = (i_nb_samples - i_end_trim) *
+                              p_sys->header.channels * sizeof(float);
+
     if( spp > i_nb_samples )
     {
         memmove(p_aout_buffer->p_buffer,
