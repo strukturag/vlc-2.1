a365ac25fbe216c99321be24e63aab99f2f37602
Felix Abecassis <felix.abecassis@gmail.com>
2014-01-24 19:14:53 +0100

    Win32: correctly use option "disable-screensaver"
    Close #10366.
    
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>
    (cherry picked from commit d38e0f861f5b7e38f52bb54b55062cfa93b28eb2)
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>
    
    Conflicts:
    	modules/video_output/msw/common.c

--- modules/video_output/msw/common.c
+++ modules/video_output/msw/common.c
@@ -78,6 +78,7 @@ int CommonInit(vout_display_t *vd)
     sys->is_on_top = false;
 
     var_Create(vd, "video-deco", VLC_VAR_BOOL | VLC_VAR_DOINHERIT);
+    var_Create(vd, "disable-screensaver", VLC_VAR_BOOL | VLC_VAR_DOINHERIT);
 
     /* */
     sys->event = EventThreadCreate(vd);
@@ -113,8 +114,6 @@ int CommonInit(vout_display_t *vd)
             vout_display_SendEventFullscreen(vd, false);
     }
 
-    /* Why not with glwin32 */
-    var_Create(vd, "disable-screensaver", VLC_VAR_BOOL | VLC_VAR_DOINHERIT);
     DisableScreensaver (vd);
 
     return VLC_SUCCESS;
